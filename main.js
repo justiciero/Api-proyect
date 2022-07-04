ae6ab38bdc8e90cdf33d54e8e746ecd181488f2c
Sam Alex <imsamalexyt@gmail.com>
2022-07-04 20:43:23 -0300

    cargamos gatos random y ademas podeis agregar a favoritos

--- main.js
+++ main.js
@@ -1,44 +1,108 @@
-const querySrting = [
+const queryString = [
     '?',
-    'limit=3',
+    'limit=2',
+    '&',
+    'api_key=cdcec2ef-c356-4097-bee3-98bc0fbdeae2'
+].join('');
+const queryString2 = [
+    '?',
+    'api_key=cdcec2ef-c356-4097-bee3-98bc0fbdeae2'
 ].join('');
 
-const URL = `https://api.thecatapi.com/v1/images/search${querySrting}`;
+const URL_RANDOM = `https://api.thecatapi.com/v1/images/search${queryString}`;
 
-const btn = document.getElementById('btn-recargar');
-    document.addEventListener('click', recargar);
+const URL_FAVOURITES = `https://api.thecatapi.com/v1/favourites${queryString2}`;
 
+const URL_DEL = `https://api.thecatapi.com/v1/favourites/{favourite_id}${queryString2}`;
 
-async function recargar(btn){
+const favoriteMichi = document.getElementById('favorite-michi');
+
+const spanError = document.getElementById('error');
+
+async function LoadRandomMichis(){
     try{
-        const resultado = await fetch(URL);
+        const resultado = await fetch(URL_RANDOM);
         const data = await resultado.json()
 
         const img1 = document.getElementById('img-random-1');
         const img2 = document.getElementById('img-random-2');
-        const img3 = document.getElementById('img-random-3');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
 
         img1.src = data[0].url;
         img2.src = data[1].url;
-        img3.src = data[2].url;
+
+        console.log(data, 'random');
+        console.log(data, 'random');
+
+        btn1.onclick = () => saveMichi(data[0].id)
+        btn2.onclick = () => saveMichi(data[1].id)
+
     } catch(error){
         alert('Este mensaje no se ha podido mostrar por un error');
         console.log(error);
     }
 }
 
-recargar();
+async function LoadFavouriteMichi(){
+    const resultado = await fetch(URL_FAVOURITES);
+    const data = await resultado.json()
+    console.log(data, 'favoritos');
+    console.log(data, 'favoritos');
+    try{
+        const michi = await saveMichi();
+        let view = `
+            ${data.map(michiup => `
+            <article id="article">
+            <div>
+                <img src='${michiup.image.url}'">
+            </div>
 
+            <button 
+                ${deleteMichi.michiup}
+            class="favorito like" id="abc"><img src="./dislike-icon.png"></div></button>
+        </article>
+            `)}
+        `
+        
+        favoriteMichi.innerHTML = view;
 
+    } catch(error){
+        console.log(error)
+    }
+}
 
-// function recargar(btn){
+async function saveMichi(id){
+    const res = await fetch(URL_FAVOURITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            image_id: id,
+        }),
+    })
+    const data = await res.json();
+    console.log(res);
     
-    //     fetch(URL)
-    //         .then(res => res.json())
-    //         .then(data =>{
-//             const img = document.querySelector('img');
-//             img.src = data[0].url;
-//         });
-// }
+    if(res.status !== 200){
+        spanError.innerHTML = 'hubo error' + res.status + data.messag
+;    }
 
-// recargar();
+}
+
+async function deleteMichi(id){
+    const del = await fetch(URL_DEL, {
+        method: 'DELETE',
+    });
+
+    const data = await del.json();
+
+    if (response.status !== 200)
+      return (spanError.innerText = "An error has occurred," + data.message);
+  
+    loadFavouriteCats();
+}
+
+LoadRandomMichis();
+LoadFavouriteMichi();
